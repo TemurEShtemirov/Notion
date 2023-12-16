3764ce5a1f6f6e5f2d5904ec4ebb877ad0717acc
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-16 21:59:46 +0500

    adding pricing card

---
+++ app/(home)/component/pricing-card.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface PricingCardProps {
+    title:string,
+    subtitle:string,
+    options:string,
+    price:string
+}
+
+export default function PricingCard() {
+  return <div>
+
+  </div>
+}
--- app/(home)/component/pricing.tsx
+++ app/(home)/component/pricing.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import PricingCard from "./pricing-card";
 
 export default function Pricing() {
   return (
@@ -13,6 +14,13 @@ export default function Pricing() {
           <Image src={team} width={50} height={50} key={idx} alt="teams" />
         ))}
       </div>
+      <div className="mt-6">
+        <div className="space-y-8 lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+{cards.map((card,idx)=>(
+    <PricingCard key={idx} {...card} />
+))}
+        </div>
+      </div>
     </div>
   );
 }
@@ -23,4 +31,30 @@ const teams = [
   "/teams/3.svg",
   "/teams/4.svg",
   "/teams/5.svg",
+];
+
+const cards = [
+  {
+    title: "Free",
+    subtitle: "For organizing every corner of your work & life.",
+    options:
+      "Collaborative workspace, Integrate with Slack, GitHub & more, Basic page analytics, 7 day page history, Invite 10 guests",
+    price: "Free",
+  },
+  {
+    title: "Plus",
+    subtitle: "A place for small groups to plan & get organized.",
+    options:
+      "Unlimited blocks for teams, Unlimited file uploads, 30 day page history, Invite 100 guests",
+    price: "8",
+    priceId: "price_1OIa9xL7w2jHXlsS4WjLBAXz",
+  },
+  {
+    title: "Business",
+    subtitle: "For companies using Notion to connect several teams & tools.",
+    options:
+      "SAML SSO, Private teamspaces, Bulk PDF export, Advanced page analytics, 90 day page history, Invite 250 guests",
+    price: "15",
+    priceId: "price_1OIa9BL7w2jHXlsS0gMw5Bob",
+  },
 ];
