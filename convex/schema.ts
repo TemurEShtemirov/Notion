9e9b65737b48a4be2412c10a535246956d2d5aa0
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-20 16:16:48 +0500

    added to define schema document

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,8 +2,16 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  messages: defineTable({
-    body: v.string(),
-    user: v.id("users"),
-  }),
+  document: defineTable({
+    title: v.string(),
+    userId: v.string(),
+    isArchived: v.boolean(),
+    parenDocument: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.boolean(),
+  })
+    .index("by_user", ["userId"])
+    .index("by_user_parent", ["userId", "parenDocument"]),
 });
