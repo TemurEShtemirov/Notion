d32b484ecf4c07fbac09d1f3bc73b518e191397e
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-21 22:14:49 +0500

    added collapse and reset to sidebar and adding handle mouse down  function

--- app/(secret)/components/sidebar.tsx
+++ app/(secret)/components/sidebar.tsx
@@ -1,14 +1,73 @@
-import { ChevronsLeft } from "lucide-react";
-import React from "react";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import React, { ElementRef, useRef, useState } from "react";
 
 export const Sidebar = () => {
+  const sidebarRef = useRef<ElementRef<"div">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isResetting, setIsResetting] = useState(false);
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.width = "100%";
+      navbarRef.current.style.left = "0";
+    }
+  };
+
+  const reset = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "240px";
+      navbarRef.current.style.width = "calc(100% -240px)";
+      navbarRef.current.style.left = "240px";
+
+    }
+  };
+
+  
+
   return (
     <>
-      <div className="group h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-50">
-        <div className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover:opacity-100 transition">
+      <div
+        className={cn(
+          "group/sidebar h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-50",
+          isResetting && "transition-all ease-in duration-300"
+        )}
+        ref={sidebarRef}
+      >
+        <div
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+          onClick={collapse}
+        >
           <ChevronsLeft className="h-6 w-6" />
         </div>
-        <div className="absolute right-0 top-0 w-1 h-full cursor-ew-resize bg-primary/10 opacity-0 group-hover:opacity-100 transition"></div>
+        <div className="absolute right-0 top-0 w-1 h-full cursor-ew-resize bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 transition" onClick={handleMouseDown}></div>
+      </div>
+      <div
+        className={cn(
+          "absolute top-0 z-50 left-60 w-[calc(100% -240px)]",
+          isResetting && "transition-all ease-in duration-300"
+        )}
+        ref={navbarRef}
+      >
+        <nav className={cn("bg-transparent px-3 py-2 w-full")}>
+          {isCollapsed && (
+            <MenuIcon
+              className="h-6 w-6 text-muted-foreground"
+              role="button"
+              onClick={reset}
+            />
+          )}
+        </nav>
       </div>
     </>
   );
--- app/(secret)/documents/page.tsx
+++ app/(secret)/documents/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DocumentPage = () => {
-  return <div>DocumentPage</div>;
+  return <div></div>;
 };
 
 export default DocumentPage;
